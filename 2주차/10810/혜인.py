5683833ab2a40d3fe0b90b376bdc95c8b384e770
haein45 <ohhaein0405@gmail.com>
2025-01-19 10:31:37 +0900

    2주차오혜인

--- 2주차/10810/혜인.py
+++ 2주차/10810/혜인.py
@@ -1,1 +1,9 @@
 n,m = map(int, input().split())
+baskets =  [0]*(n+1)
+
+for _ in range(m):
+    a,b,c = map(int, input().split())
+    for i in range(a,b+1) :
+        baskets[i] = c
+for j in range(1, len(baskets)):
+    print(baskets[j], end=' ')
