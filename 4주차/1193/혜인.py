71edbbac19621e89c80ff74f67f847f4d17626a4
haein45 <ohhaein0405@gmail.com>
2025-02-02 17:27:18 +0900

    4주차 제출출

---
+++ 4주차/1193/혜인.py
@@ -0,0 +1,18 @@
+# 1/1
+# 1/2, 2/1
+# 3/1, 2/2, 1/3
+# 1/4, 2/3, 3/2, 4/1
+# 5/1, 4/2, 3/3, 2/4, 1/5
+count = 1
+input1 = int(input())
+while input1 - count > 0 :
+    input1 -= count
+    count += 1
+
+if count % 2 == 0:
+    print(f"{input1}/{count - input1 + 1}")
+else:
+    print(f"{count - input1 + 1}/{input1}")
+
+
+
---
+++ 4주차/2745/혜인.py
@@ -0,0 +1,12 @@
+N, B_str = input().split()
+list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+B = int(B_str)
+total = 0
+b = int(len(N)) 
+for i in range(0,b):
+    c = b - i - 1
+    a = N[c]
+    list_index = list.index(a)
+    total += ((B ** i) *  list_index)
+
+print(total)
