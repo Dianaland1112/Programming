6abd48b446a1922052ca69a6001af9b08837a2f6
haein45 <ohhaein0405@gmail.com>
2025-02-04 21:48:35 +0900

    4주차

--- 4주차/2563/혜인.py
+++ 4주차/2563/혜인.py
@@ -1,15 +1,15 @@
-def checkarea(x1, y1, x2, y2):
-    n = 0
-    if abs(x1 - x2) < 10 or abs(y1 - y2) < 10:
-        area = (abs(x1 - x2)) * (abs(y1 - y2))
-        n += 1
-    print(area)
+n = int(input())
+grid = [[0] * 100 for _ in range(100)]
 
-listx = {(x1 + 10), (x2 + 10), (x3 + 10), (x1 + 10)}
-listy = {(y1 + 10), (y2 + 10), (y3 + 10), (y1 + 10)}
-area_total = 0
-for i in range :
-    checkarea(listx[i], listy[i], listx[i+1], listy[i+1])
-    area_total += area
-    if n == 3:
-        (max(listx[i]) - median(listx[i])) * (max(listx[y) - median(listx[i]))
+for _ in range(n):
+    x,y = map(int, input().split())
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            grid[i][j] = 1
+            
+total = 0
+for row in grid:
+    total += sum(row)
+
+print(total)
+
